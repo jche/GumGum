0e97ef0d9e527c18cc3ce178e63bb62fa8f94345
Shurooo <wl1191@nyu.edu>
2016-07-11 10:13:37 -0700

    Fix Sparse_Matrix_IO

--- Sparse_Matrix_IO.py
+++ Sparse_Matrix_IO.py
@@ -4,9 +4,10 @@ from scipy.sparse import csr_matrix
 
 def save_sparse_csr(filename, array):
     array_sparse = csr_matrix(array)
-    np.savez(filename, data=array.data, indices=array.indices, indptr=array.indptr, shape=array.shape)
+    np.savez(filename, data=array_sparse.data, indices=array_sparse.indices, indptr=array_sparse.indptr, shape=array_sparse.shape)
 
 
 def load_sparse_csr(filename):
     loader = np.load(filename)
     return csr_matrix((loader['data'], loader['indices'], loader['indptr']), shape=loader['shape']).toarray()
+
