b6655c7f85a3aa084f83aa404459e10cb2d308aa
jche <jonche10@gmail.com>
2016-07-29 13:38:06 -0700

    XGB with and without scikit wrapper

--- XGBoost.py
+++ XGBoost.py
@@ -1,11 +1,12 @@
 import xgboost as xgb
 import numpy as np
 from scipy.sparse import csr_matrix
-from sklearn.metrics import confusion_matrix, recall_score
-import operator
-import pandas as pd
-from matplotlib import pylab as plt
-from sklearn.preprocessing import LabelEncoder
+from sklearn import metrics
+import time
+try:
+    import cPickle as pickle
+except:
+    import pickle
 
 
 # XGBoost 101 found at http://xgboost.readthedocs.io/en/latest/python/python_intro.html
@@ -23,54 +24,46 @@ def format_data(data):
     m = int(np.size(d,1))   # Number of columns
     n = int(np.size(d,0))   # Number of rows
     print "There are %s data points, each with %s features" % (n, m-1)
-    x = d[:, :m-1]
-    y = d[:, m-1]
+    x = d[:200000, :m-1]   # To reduce load on computer
+    y = d[:200000, m-1]
     return x, y
-
-
-def recall(preds, dtrain):
-    cutoff = 0.1
-    labels = dtrain.get_label()
-    # return a pair metric_name, result
-    # since preds are margin(before logistic transformation, cutoff at 0)
-    preds_bin = np.greater(preds, np.zeros(len(labels))+cutoff)
-    return "recall", recall_score(labels, preds_bin)
 
 
 if __name__ == "__main__":
     # Inputting training and testing set
-    train_data, train_label = format_data("/home/jche/Data/day_samp_bin_0604.npy")
+    train_data, train_label = format_data("/home/jche/Data/alldata3_bin.npy")
     dtrain = xgb.DMatrix(train_data, label=train_label)
-    test_data, test_label = format_data("/home/jche/Data/day_samp_bin_0605.npy")
+    test_data, test_label = format_data("/home/jche/Data/alldata5_bin.npy")
     dtest = xgb.DMatrix(test_data, label=test_label)
 
     # Setting parameters
     param = {'booster':'gbtree',   # Tree, not linear regression
              'objective':'binary:logistic',   # Output probabilities
-             'bst:max_depth':4,   # Max depth of tree
-             'bst:eta':.5,   # Learning rate (usually 0.01-0.2)
-             'silent':0,   # 0 outputs messages, 1 does not
-             'nthread':4}    # Number of cores used; otherwise, auto-detect
-    #param['eval_metric'] = 'error'
+             'bst:max_depth':5,   # Max depth of tree
+             'bst:eta':.1,   # Learning rate (usually 0.01-0.2)
+             'bst:gamma': 1,   # Larger value --> more conservative
+             'silent':1,   # 0 outputs messages, 1 does not
+             'save_period':0,   # Only saves last model
+             'nthread':8}    # Number of cores used; otherwise, auto-detect
+    param['eval_metric'] = ['error', 'logloss']
     evallist = [(dtest,'eval'), (dtrain,'train')]
 
     num_round = 100   # Number of rounds of training, increasing this increases the range of output values
-    #bst = xgb.train(param, dtrain, num_round, evallist, feval=recall, maximize=True)
     bst = xgb.train(param,
                     dtrain,
                     num_round,
                     evallist,
                     early_stopping_rounds=10)   # If error doesn't decrease in n rounds, stop early
-    bst.dump_model('dump.raw.txt')
+    bst.dump_model('/home/jche/Desktop/dump.raw.txt')
+    # bst.save_model('/home/jche/Desktop/0001.model')
+    # bst = xgb.Booster({'nthread':4}) #init model
+    # bst.load_model("model.bin") # load data
 
     y_true = test_label
     y_pred = bst.predict(dtest)
-    for cutoff in range(1, 10):
-        cut = cutoff/float(10)   # Cutoff, checking from .1 thru .9
+    for cutoff in range(0, 31):
+        cut = cutoff/float(100)   # Cutoff in decimal form
         print "Cutoff is: %s" % cut
-        y = np.greater(y_pred, np.zeros(len(y_true))+cut)   # If y values are greater than the cutoff
-        print "Recall is: %s" % recall_score(y_true, y)
-        print confusion_matrix(y_true, y)
-
-    #xgb.plot_importance(bst, xlabel="test")
-    #xgb.plot_tree(bst, num_trees=2)
+        y = y_pred > cut   # If y values are greater than the cutoff
+        print "Recall is: %s" % metrics.recall_score(y_true, y)
+        print metrics.confusion_matrix(y_true, y)
