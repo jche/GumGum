a880e0624aa84e16f70b99a413145776271337f9
kbhalla <krishanbhalla1@gmail.com>
2016-07-29 10:49:26 -0700

    I was being dumb, removed previous change

--- Sparse_Matrix_IO.py
+++ Sparse_Matrix_IO.py
@@ -10,11 +10,3 @@ def save_sparse_csr(filename, array):
 def load_sparse_csr(filename):
     loader = np.load(filename)
     return csr_matrix((loader['data'], loader['indices'], loader['indptr']), shape=loader['shape']).toarray()
-
-def load_sparse_csr_npz(filename):
-    with np.load(filename) as Data:
-        data = Data['data']
-        indices = Data['indices']
-        indptr = Data['indptr']
-        shape = Data['shape']
-        return csr_matrix((data, indices, indptr), shape=shape).toarray()
