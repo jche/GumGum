2789b58440a34d1c4f779d22a779986f3fc38e18
kbhalla <krishanbhalla1@gmail.com>
2016-08-01 15:16:15 -0700

    "Clusters" added to path for relevant case

--- Get_Data.py
+++ Get_Data.py
@@ -85,11 +85,11 @@ def get(addr_day, cluster_number = -1, ratio=-1, features_to_get=None):
         neg = int(n / (1+ratio))
         pos = n - neg
         if cluster_number != -1:
-            with open(os.path.join(addr_day, "day_samp_new_neg_" + str(cluster_number) + ".npz"), "r") as file_neg:
+            with open(os.path.join(addr_day,"Clusters", "day_samp_new_neg_" + str(cluster_number) + ".npz"), "r") as file_neg:
                 matrix_neg = smio.load_sparse_csr(file_neg)
                 print "Negative Cluster %s Loaded" % cluster_number
             matrix_neg = matrix_neg[:, :]
-            with open(os.path.join(addr_day, "day_samp_new_pos_" + str(cluster_number) + ".npz"), "r") as file_pos:
+            with open(os.path.join(addr_day,"Clusters", "day_samp_new_pos_" + str(cluster_number) + ".npz"), "r") as file_pos:
                 matrix_pos = smio.load_sparse_csr(file_pos)
                 print "Positive Cluster %s Loaded" % cluster_number
             matrix_pos = matrix_pos[:, :]
